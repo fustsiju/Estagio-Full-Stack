58136a5edf39ee1a2d7cc94f55aab3d23607edf8
Fulvio Stefanine <62194735+fustsiju@users.noreply.github.com>
2024-11-27 15:26:47 -0300

    Update script.js

--- Desafio 2 - Busca Estagiario/script.js
+++ Desafio 2 - Busca Estagiario/script.js
@@ -7,8 +7,13 @@ let final;
 let quant = 0;
 let resultado = [];
 let pag = [{}];
-let minimo = 1;
+let minimo = 0;
 let maximo = 10;
+let paginas = [];
+let qtdPagina = 0;
+let paginaAtual = 1;
+let paginacaoInicial = 0;
+let paginacaoFinal = 1;
 let naoEncontrado = `<tr><td colspan="8" style="text-align: center;">Nenhum Estagiário Encontrado.</td></tr>`;
 $("#dataIni").mask("00/00/0000");
 $("#dataFim").mask("00/00/0000");
@@ -20,22 +25,42 @@ function converterEntradas() {
 }
 
 $("#busca").on("click", function () {
-  minimo = 1;
+  minimo = 0;
   maximo = 10;
+  qtdPagina = 0;
+  paginas = [];
+  quant = 0;
+  resultado = [];
+  paginacaoInicial = 0;
+  paginacaoFinal = 1;
+
+  $("tbody").remove();
+  $("table").append("<tbody></tbody>");
+  $("tbody").append(naoEncontrado);
+  $("#proximo").css({
+    display: "flex",
+    color: "black",
+    "pointer-events": "all",
+  });
   $(".navg").css("display", "flex");
   $("#add").css("width", "50px");
   $("#anterior").css("display", "none");
   $("#proximo").css("display", "flex");
-  resultado = [];
+  $("#add").text(paginacaoInicial * 10 + 1 + "-" + paginacaoFinal * 10);
   converterEntradas();
   buscaEstagiario();
-  trocaPag(minimo, maximo);
-  geraTabela(minimo, maximo);
-  $("#add").text(minimo + "-" + maximo);
+  trocaPag();
+  console.log(paginas);
 });
 
+function numerosPaginacao() {
+  $("#add").text(paginacaoInicial * 10 + 1 + "-" + paginacaoFinal * 10);
+  if (paginaAtual === qtdPagina) {
+    $("#add").text(paginacaoInicial * 10 + 1 + "-" + quant);
+  }
+}
+
 function buscaEstagiario() {
-  quant = 0;
   $("tbody").empty();
   pegaValorInputs();
   for (let p = 0; p < dados.length; p++) {
@@ -43,113 +68,116 @@ function buscaEstagiario() {
     pag[p].DataInicio = pag[p].DataInicio.split("-").reverse().join("/");
     pag[p].DataFim = pag[p].DataFim.split("-").reverse().join("/");
     if (
-      (nome === "" || nome === pag[p].Nome) &&
+      (nome === "" ||
+        nome === pag[p].Nome.toLowerCase() ||
+        pag[p].Nome.toLowerCase().includes(nome)) &&
       (cidade === "" || cidade === pag[p].Cidade) &&
-      (inicial === "" || inicial === pag[p].DataInicio.split("-").reverse().join("/")) &&
+      (inicial === "" ||
+        inicial === pag[p].DataInicio ||
+        pag[p].DataInicio.includes(inicial)) &&
       (final === "" || final === pag[p].DataFim.split("-").reverse().join("/"))
     ) {
-      quant++;
       resultado[quant] = pag[p];
+      quant++;
+      if (quant % 10 === 0) {
+        paginas[qtdPagina] = resultado.slice(minimo, maximo);
+        minimo += 10;
+        maximo += 10;
+        qtdPagina++;
+      }
     }
+  }
+
+  if (maximo > quant) {
+    maximo = quant;
+    paginas[qtdPagina] = resultado.slice(minimo, maximo);
+  }
+  if (quant <= 10) {
+    $(".navg").css("display", "none");
   }
 }
 
-function geraTabela(min, max) {
-  quant = 0;
+function geraTabela(lista) {
   $("tbody").empty();
   pegaValorInputs();
-
   if (resultado.length === 0) {
     $("tbody").append(naoEncontrado);
     return;
   }
 
-  for (let o = min; o < resultado.length; o++) {
-    if (o <= max) {
-      $("tbody").append(`<tr><td >${resultado[o].Codigo}</td>
-       <td >${resultado[o].Nome}</td>
-       <td >${resultado[o].Cidade}</td>
-       <td >${resultado[o].DataInicio}</td>
-       <td >${resultado[o].DataFim}</td>
-       <td >${resultado[o].Cargo}</td>
-       <td >${resultado[o].Responsavel}</td>
-       <td >${resultado[o].Efetivo}</td></tr>`);
-    }
-  }
-  if (resultado.length < 10) {
-    $(".navg").css("display", "none");
-  } else {
-    $(".navg").css("display", "flex");
-  }
+  paginas[lista].forEach((item) => {
+    $("tbody").append(`<tr>
+      <td>${item.Codigo}</td>
+      <td>${item.Nome}</td>
+      <td>${item.Cidade}</td>
+      <td>${item.DataInicio}</td>
+      <td>${item.DataFim}</td>
+      <td>${item.Cargo}</td>
+      <td>${item.Responsavel}</td>
+      <td>${item.Efetivo}</td>
+    </tr>`);
+  });
+}
 
+if (resultado.length < 10) {
+  $(".navg").css("display", "none");
+} else {
+  $(".navg").css("display", "flex");
 }
 
 function pegaValorInputs() {
-  nome = $("#nomeEstagiario").val();
+  nome = $("#nomeEstagiario").val().toLowerCase();
   cidade = $("#cidadeEstagiario").val();
   inicial = $("#dataIni").val();
   final = $("#dataFim").val();
   $("#proximo").css({ "pointer-events": "all" });
 }
 
-function trocaPag(minimo, maximo) {
-  if (maximo <= pag.length) {
-    $("#proximo").on("click", function () {
-      if (maximo >= resultado.length - 11) {
-        $("#proximo").css("display", "none");
-        $("#add").css("margin-left", "24px");
-      }
-      $("#anterior").css({ display: "flex" });
-      $("tbody").empty();
-      if (maximo === resultado.length) {
-        $("#add").text(minimo + 1 + "-" + maximo);
-        geraTabela(minimo + 1, maximo);
-      } else if (maximo < resultado.length) {
-        maximo += 10;
-        minimo = maximo - 10;
-        $("#add").text(minimo + 1 + "-" + maximo);
-        geraTabela(minimo + 1, maximo);
-        if (maximo >= resultado.length) {
-          $("tbody").empty();
-          maximo = resultado.length - 1;
-          if (minimo === maximo) {
-            minimo = minimo - 10;
-          } else {
-            minimo = minimo;
-          }
-          $("#add").text(minimo + 1 + "-" + maximo);
-          geraTabela(minimo + 1, maximo);
+function trocaPag() {
+  paginaAtual = 0;
+  geraTabela(0);
+  $("#proximo")
+    .off("click")
+    .on("click", function () {
+      paginacaoInicial++;
+      paginacaoFinal++;
+      if (paginaAtual <= qtdPagina - 1) {
+        paginaAtual++;
+        geraTabela(paginaAtual);
+        console.log(paginaAtual);
+        if (paginaAtual >= qtdPagina - 1) {
+          $("#proximo").css({ "pointer-events": "none", color: "white" });
         }
       }
+      $("#anterior").css({
+        display: "flex",
+        color: "black",
+        "pointer-events": "all",
+      });
+      numerosPaginacao();
     });
-  }
 
-  if (minimo >= 0) {
-    $("#anterior").on("click", function () {
-      if (minimo <= 11) {
-        $("#anterior").css("display", "none");
-        $("#add").css("margin-left", "24px");
+  $("#anterior")
+    .off("click")
+    .on("click", function () {
+      paginacaoInicial--;
+      paginacaoFinal--;
+      if (paginaAtual >= 0) {
+        paginaAtual--;
+        geraTabela(paginaAtual);
+        console.log(paginaAtual);
+        if (paginaAtual == 0) {
+          $("#anterior").css({ "pointer-events": "none", color: "white" });
+        }
       }
-      $("#add").css("margin-left", "0px");
-      $("tbody").empty();
-      $("#proximo").css("display", "flex");
-      $("#add").css("width", "50px");
-      if (minimo === 0 || minimo < 0) {
-        minimo === 0;
-        maximo === 10;
-        $("#add").text(minimo + 1 + "-" + maximo);
-        geraTabela(minimo + 1, maximo);
-      } else if (maximo % 10 == 0) {
-        minimo -= 10;
-        maximo -= 10;
-        $("#add").text(minimo + 1 + "-" + maximo);
-        geraTabela(minimo + 1, maximo);
-      } else {
-        minimo = 0;
-        maximo = 10;
-        $("#add").text(minimo + 1 + "-" + maximo);
-        geraTabela(minimo + 1, maximo);
-      }
+      $("#proximo").css({
+        display: "flex",
+        color: "black",
+        "pointer-events": "all",
+      });
+      numerosPaginacao();
     });
+  if (quant % 10 !== 0) {
+    paginas[qtdPagina] = resultado.slice(minimo, quant);
   }
 }
