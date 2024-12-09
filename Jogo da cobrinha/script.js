853f9040fc9959d5c79a44a2690e713c126cb1c8
Fulvio Stefanine <62194735+fustsiju@users.noreply.github.com>
2024-12-09 14:26:50 -0300

    Update script.js

--- Jogo da cobrinha/script.js
+++ Jogo da cobrinha/script.js
@@ -1,7 +1,7 @@
 let cobra = [
   { x: 150, y: 150 },
-  { x: 120, y: 150 }, 
-  { x: 90, y: 150 }, 
+  { x: 120, y: 150 },
+  { x: 90, y: 150 },
 ];
 let posicaoX = 0;
 let posicaoY = 0;
@@ -9,64 +9,78 @@ const velocidade = 30;
 let direcao = "direita";
 let anguloAtual = 0;
 let posicaoComidaX = 0;
-let posicaoComidaY = 0; 
+let posicaoComidaY = 0;
 let pontuacaoComida = 0;
 let emJogo = 0;
 let attPosicao;
-let attComida;
+let attVerificacoes;
 let widTam = 90;
-let rabo = [{}];
-let tempoProximaTecla = 200;
-let tempo = 0;
 let ultimaTecla = 0;
 const intervaloMinimo = 70;
+let obstaculo = [];
+let aumentoVelocidade = 100;
+let maximoPontuacao = 25;
 
 function menu() {
+  $("img, h1").remove();
+
   $(".container").append(
     "<img src='./img/cobrinha.png' height='300px'><h1>ESPAÇO PARA INICIAR O JOGO</h1>"
   );
-  $("#pontComida").remove();
-  $(".comida").remove();
-  $(".cobra").remove();
-  $("#cobrinha").remove();
-  document.addEventListener("keydown", function (event) {
-    
+
+  $("#pontComida, .comida, .cobra, #cobrinha, .obstaculo").remove();
+
+  $(document).on("keydown", function (event) {
     if (event.keyCode == 32 && emJogo === 0) {
-      $(".container").css({
-        display: "grid",
-        "grid-template-rows": "repeat(20,auto)",
-        "grid-template-columns": "repeat(20, auto)",
-      });
-      $(".container").append(
-        "<div class='pontuacao' id='pontComida'>0</div> <div class='cobra'  ><div class='cabeca' id='cobrinha'></div></div>"
-      );
-      $("img").remove();
-      $("h1").remove();
+      $(".vitoria, h2").remove();
+      $(".container")
+        .css({
+          display: "grid",
+          "grid-template-rows": "repeat(20, auto)",
+          "grid-template-columns": "repeat(20, auto)",
+        })
+        .append(
+          "<div class='pontuacao' id='pontComida'>0</div> <div class='cobra'><div class='cabeca' id='cobrinha'></div></div>"
+        );
+      $("img, h1").remove();
       emJogo = 1;
-      
       movimentacao();
+      geraObstaculo(3);
       geraComida();
-      attPosicao = setInterval(atualizarPosicao, 100);
-      attComida = setInterval(comeuComida, 1);
+
+      attPosicao = setInterval(atualizarPosicao, aumentoVelocidade);
+      attVerificacoes = setInterval(verificarJogo, aumentoVelocidade);
     }
   });
 }
 
 function movimentacao() {
-  document.addEventListener("keydown", function (event) {
+  $(document).on("keydown", function (event) {
     let tempoAtual = new Date().getTime();
 
     if (tempoAtual - ultimaTecla > intervaloMinimo) {
-      if (event.key === "ArrowRight" && direcao !== "esquerda") {
+      if (event.key === "ArrowRight" && direcao !== "esquerda" && emJogo == 1) {
         direcao = "direita";
         anguloAtual = 0;
-      } else if (event.key === "ArrowDown" && direcao !== "cima") {
+      } else if (
+        event.key === "ArrowDown" &&
+        direcao !== "cima" &&
+        emJogo == 1
+      ) {
         direcao = "baixo";
         anguloAtual = 90;
-      } else if (event.key === "ArrowLeft" && direcao !== "direita") {
+      } else if (
+        event.key === "ArrowLeft" &&
+        direcao !== "direita" &&
+        emJogo == 1
+      ) {
         direcao = "esquerda";
         anguloAtual = 180;
-      } else if (event.key === "ArrowUp" && direcao !== "baixo") {
+      } else if (
+        event.key === "ArrowUp" &&
+        direcao !== "baixo" &&
+        emJogo == 1
+      ) {
         direcao = "cima";
         anguloAtual = -90;
       }
@@ -80,35 +94,41 @@ function movimentacao() {
 }
 
 function reiniciar() {
-  clearInterval(attComida);
   clearInterval(attPosicao);
+  clearInterval(attVerificacoes);
+  aumentoVelocidade = 100;
   emJogo = 0;
   posicaoX = 150;
   posicaoY = 150;
   direcao = "direita";
+
   cobra = [
     { x: 150, y: 150 },
-    { x: 120, y: 150 }, 
-    { x: 90, y: 150 }, 
+    { x: 120, y: 150 },
+    { x: 90, y: 150 },
   ];
+  if (pontuacaoComida != maximoPontuacao) {
+    menu();
+  }
   pontuacaoComida = 0;
-  menu();
 }
 
 function atualizarPosicao() {
   let novaPosicao = { ...cobra[0] };
 
-  if (direcao === "direita") {
-    novaPosicao.x += velocidade;
-  }
-  if (direcao === "baixo") {
-    novaPosicao.y += velocidade;
-  }
-  if (direcao === "esquerda") {
-    novaPosicao.x -= velocidade;
-  }
-  if (direcao === "cima") {
-    novaPosicao.y -= velocidade;
+  switch (direcao) {
+    case "direita":
+      novaPosicao.x += velocidade;
+      break;
+    case "baixo":
+      novaPosicao.y += velocidade;
+      break;
+    case "esquerda":
+      novaPosicao.x -= velocidade;
+      break;
+    case "cima":
+      novaPosicao.y -= velocidade;
+      break;
   }
 
   cobra.unshift(novaPosicao);
@@ -129,35 +149,69 @@ function atualizarPosicao() {
 
     if (index !== 0 && cobra[0].x === segmento.x && cobra[0].y === segmento.y) {
       reiniciar();
+      return;
+    } else if (
+      novaPosicao.x >= 600 ||
+      novaPosicao.y >= 600 ||
+      novaPosicao.x < 0 ||
+      novaPosicao.y < 0
+    ) {
+      reiniciar();
     }
   });
-
-  if (
-    novaPosicao.x >= 600 ||
-    novaPosicao.y >= 600 ||
-    novaPosicao.x < 0 ||
-    novaPosicao.y < 0
-  ) {
-    reiniciar();
-  }
 }
 
-function inteiroRandom(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
+function inteiroRandomObstaculo(min, max) {
+  validacaoX = Math.floor(Math.random() * (max - min + 1) + min) * 30;
+  while (validacaoX !== 150) {
+    if (validacaoX === 150) {
+      validacaoX = Math.floor(Math.random() * (max - min + 1) + min) * 30;
+    }
+    return validacaoX;
+  }
+}
+function inteiroRandomComida(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min) * 30;
+}
+
+function geraPosicao(geraQuant) {
+  obstaculo = [];
+  for (let h = 0; h < geraQuant; h++) {
+    let posicaoUnica = false;
+    let posX, posY;
+    while (!posicaoUnica) {
+      posX = inteiroRandomObstaculo(1, 19);
+      posY = inteiroRandomObstaculo(1, 19);
+      posicaoUnica =
+        !obstaculo.some(
+          (posicao) => posicao[0] === posX && posicao[1] === posY
+        ) &&
+        !obstaculo.some(
+          (obstaculoPos) =>
+            obstaculoPos[0] === posicaoComidaX &&
+            obstaculoPos[1] === posicaoComidaY
+        );
+    }
+    obstaculo.push([posX, posY]);
+  }
 }
 
 function geraComida() {
   let comidaValida = false;
   while (!comidaValida) {
-
-    posicaoComidaX = inteiroRandom(1, 19) * 30;
-    posicaoComidaY = inteiroRandom(1, 19) * 30;
-
-    comidaValida = !cobra.some(
-      (segmento) => segmento.x === posicaoComidaX && segmento.y === posicaoComidaY
-    );
+    posicaoComidaX = inteiroRandomComida(1, 19);
+    posicaoComidaY = inteiroRandomComida(1, 19);
+    comidaValida =
+      !cobra.some(
+        (segmento) =>
+          segmento.x === posicaoComidaX && segmento.y === posicaoComidaY
+      ) &&
+      !obstaculo.some(
+        (obstaculoPos) =>
+          obstaculoPos[0] === posicaoComidaX &&
+          obstaculoPos[1] === posicaoComidaY
+      );
   }
-
   $(".container").append("<div class='comida'></div>");
   $(".comida").css({
     top: `${posicaoComidaY}px`,
@@ -165,30 +219,62 @@ function geraComida() {
   });
 }
 
+function geraObstaculo(quantObstaculo) {
+  geraPosicao(quantObstaculo);
+
+  for (let i = 0; i < quantObstaculo; i++) {
+    $(".container").append(`<div class="obstaculo" id="obstaculo-${i}"></div>`);
+    $("#obstaculo-" + i).css({
+      top: `${obstaculo[i][1]}px`,
+      left: `${obstaculo[i][0]}px`,
+    });
+  }
+}
+
+function verificarJogo() {
+  bateuObstaculo();
+  comeuComida();
+  vitoria();
+}
+
+function vitoria() {
+  if (pontuacaoComida === maximoPontuacao) {
+    $("#pontComida, .comida, .cobra, #cobrinha, .obstaculo").remove();
+    emJogo = 0;
+    reiniciar();
+    $(".container").append(
+      "<img src='./img/cobrinha.png' height='300px'><h1 id='venceu'>VENCEU!!!</h1><h2>ESPAÇO PARA JOGAR NOVAMENTE</h2>"
+    );
+  }
+  $(".vitoria").on("click", function () {
+    reiniciar();
+  });
+}
+
+function bateuObstaculo() {
+  let bateuCabeca = cobra[0];
+  obstaculo.forEach((obst) => {
+    if (bateuCabeca.x === obst[0] && bateuCabeca.y === obst[1]) {
+      reiniciar();
+      return;
+    }
+  });
+}
 
 function comeuComida() {
   let cabeca = cobra[0];
-  if (
-    (cabeca.x === posicaoComidaX && cabeca.y === posicaoComidaY) ||
-    (cabeca.y === posicaoComidaX && cabeca.x === posicaoComidaY)
-  ) {
-    tamanhoCorpo();
+  if (cabeca.x === posicaoComidaX && cabeca.y === posicaoComidaY) {
     cobra.push({ ...cobra[cobra.length - 1] });
     pontuacaoComida += 1;
     $(".comida").remove();
     geraComida();
     $("#pontComida").text(pontuacaoComida);
-  } else if (
-    (segmento.x === posicaoComidaX && segmento.y === posicaoComidaY) ||
-    (segmento.y === posicaoComidaX && segmento.x === posicaoComidaY) 
-  ) {
-    geraComida();
+    clearInterval(attPosicao);
+    clearInterval(attVerificacoes);
+    aumentoVelocidade = aumentoVelocidade - 2;
+    attPosicao = setInterval(atualizarPosicao, aumentoVelocidade);
+    attVerificacoes = setInterval(verificarJogo, aumentoVelocidade);
   }
-}
-
-function tamanhoCorpo() {
-  widTam += 30;
-  $(".cobra").css("width", `${widTam}px`);
 }
 
 menu();
